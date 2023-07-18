3cb90269aea55c4a6e39fb1fee57bddd60493b1e
lil-m00ha <anastasiia.n.minaeva@gmail.com>
2023-07-18 13:12:10 +0500

    Added HTML elements, meta tags and images

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+export default function Footer() {
+    return (
+        <footer>
+            <img
+                alt="Footer"
+                src="/restauranfood.jpg"
+            />
+            <ul>
+                <li><b>Doormat navigation</b></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Reservations</a></li>
+                <li><a href="#">Order online</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+            <ul>
+                <li><b>Contact</b></li>
+                <li><a href="#">Address</a></li>
+            </ul>
+            <ul>
+                <li><b>Social Media links</b></li>
+                <li><a href="#">Address</a></li>
+                <li><a href="#">Phone</a></li>
+                <li><a href="#">Email</a></li>
+            </ul>
+        </footer>
+    );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <img
+                alt="Little Lemon logo"
+                src="/Logo.svg"
+            />
+        </header>
+    );
+}
